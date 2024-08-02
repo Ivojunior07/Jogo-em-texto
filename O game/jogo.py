4b38a8842b3d4cb533ce5a3c4b6107f2e9d7156d
Ivojunior07 <IvonaldoJunior07@gmail.com>
2024-08-02 14:35:42 -0300

    Adicionado as primeiras escolhas no game

--- O game/jogo.py
+++ O game/jogo.py
@@ -1,2 +1,10 @@
-input(print 'Deseja iniciar o jogo ? ')
-    if
+ Nome = input('Diga seu nome ? ')
+
+print (f'Então {Nome} acorda em sua casa como mais um dia!')
+decisaocama input('Levantar da cama ?')
+
+if decisaocama == 'Sim'
+    print(f'{Nome} levanta de sua cama e percebe algo estranho')
+
+else:
+    print(f'{Nome} decide continuar deitado e tirar mais um leve cochilo')
